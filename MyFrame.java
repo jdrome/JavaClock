87956e8e5a2e18ad6513a66c615ad4d1c385f0a5
Jose D. Romero <josedanilromero@gmail.com>
2024-04-29 06:54:35 -0500

    Commit 3 - MyFrame class constructor

--- MyFrame.java
+++ MyFrame.java
@@ -1,5 +1,14 @@
 import javax.swing.JFrame;
+import java.awt.FlowLayout;
 
 public class MyFrame extends JFrame {
-
+    // MyFrame Constructor
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Clocked");
+        this.setLayout(new FlowLayout());
+        this.setSize(350, 200);
+        this.setResizable(false);
+        this.setVisible(true);
+    }
 }
