c71a3aa33160d1d1f42ff85fcc4b6073e7912c27
Jose D. Romero <josedanilromero@gmail.com>
2024-04-30 10:45:34 -0500

    Commit 5 - Added Calendar, SimpleDateFormat, and JLabel instances to MyFrame

--- MyFrame.java
+++ MyFrame.java
@@ -1,7 +1,14 @@
 import javax.swing.JFrame;
 import java.awt.FlowLayout;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import javax.swing.JLabel;
 
 public class MyFrame extends JFrame {
+    Calendar calendar;
+    SimpleDateFormat timeFormat;
+    JLabel timeLabel;
+
     // MyFrame Constructor
     MyFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
